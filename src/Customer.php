4137843b90c40b10030880e64e1836d499910040
Muhammad Ferly Ogianta <mfogianta@gmail.com>
2022-02-18 23:59:29 +0700

    Contoh library sederhana

---
+++ src/Customer.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace ProgrammerHandal\Belajar;
+
+class Customer
+{
+    private string $name;
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function sayHello(string $name): string
+    {
+        return "Hello $name, my name is $this->name";
+    }
+}
